2d5c7c2ff6c827522a2a862756993379e7d57dfa
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2024-03-13 20:25:14 +0800

    Update ban_ips.sh

--- ban_ips.sh
+++ ban_ips.sh
@@ -16,6 +16,7 @@ if [ "$1" == "-g" ];then
 	curl "https://api.steampowered.com/ISteamApps/GetSDRConfig/v1/?appid=730" -o ${script_dir}/cs2_ips.json
 	if [ -f ${script_dir}/cs2_ips.json ];then
 		region_code=$( sh JSON.sh -b < ${script_dir}/cs2_ips.json | awk -v regex="${target_country_name}" -F'"' '( $6 == "desc" && $8 ~ regex ) {print $4}' | sed ':a;N;$!ba;s/\n/|/g' )
+		echo ${region_code}
 		sh JSON.sh -b < ${script_dir}/cs2_ips.json | awk -v regex=${region_code} -F'"' '( $4 ~ regex && $6 == "relays" && $8 == "ipv4" && $12 != "port_range" ) {print $10}' > ${script_dir}/cs2_ips
 		rm ${script_dir}/cs2_ips.json
 	fi
@@ -24,14 +25,14 @@ fi
 
 if [ "$1" == "-b" ] && [ -f ${script_dir}/$2 ];then
 	while read IPset;do
-		iptables -C FORWARD -s $IPset -j DROP||iptables -I FORWARD -s $IPset -j DROP
+		iptables -C FORWARD -s $IPset -j DROP 2>/dev/null||iptables -I FORWARD -s $IPset -j DROP 2>/dev/null
 	done < ${script_dir}/$2
 	exit 0
 fi
 
 if [ "$1" == "-u" ] && [ -f ${script_dir}/$2 ];then
 	while read IPset;do
-		iptables -D FORWARD -s $IPset -j DROP
+		iptables -D FORWARD -s $IPset -j DROP 2>/dev/null
 	done < ${script_dir}/$2
 	exit 0
 fi
